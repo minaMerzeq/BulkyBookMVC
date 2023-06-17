cdcf7ebf69c9bcb2c766ea69efeb13cd8b9e3e51
minaMerzeq <minamerzekluffy@gmail.com>
2023-06-17 16:14:06 +0300

    create 1st model

---
+++ BulkyBookWeb/Models/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
