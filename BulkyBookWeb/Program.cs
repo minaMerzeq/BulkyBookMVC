9227665a4d152a70ee2d49e03cd95682fbfc6936
minaMerzeq <minamerzekluffy@gmail.com>
2023-06-17 16:15:15 +0300

    configure appsettings to reflect to development environmnet

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// configure the appsettings to reflect different environments
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
