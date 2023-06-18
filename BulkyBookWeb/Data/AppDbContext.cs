b642c123b0346bd6d4c5fef792d34452ad087497
minaMerzeq <minamerzekluffy@gmail.com>
2023-06-18 14:23:51 +0300

    configure db context to use sql server and specify the connection string

---
+++ BulkyBookWeb/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> categories { get; set; }
+    }
+}
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,3 +1,6 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // configure the appsettings to reflect different environments
@@ -9,6 +12,9 @@ builder.Configuration
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
